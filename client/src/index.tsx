31351c18f33556d453830735784e4fef402343d0
elioh505 <elioh505@student.liu.se>
2024-04-11 17:21:12 +0200

    initial frontend code and tailwind setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import SignIn from './routes/signin/signin';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/signin',
+    element: <SignIn />
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ client/src/routes/signin/signin.tsx
@@ -0,0 +1,20 @@
+//import React from 'react';
+//import logo from './logo.svg';
+import './signin.scss';
+
+function SignIn() {
+  
+  return (
+    <>
+      <div className="sign-in-container">
+        <h1 className='text-3xl font-bold underline text-slate-200'>Sign in</h1>
+        <input type="text" />
+        <form action="on"></form>
+      </div>
+    </>
+  );
+}
+
+
+
+export default SignIn;
