0f179ff01c065d758d58257afd211f15547e709f
jacko460 <jacko460@student.liu.se>
2024-04-23 13:16:55 +0200

    update table name for easier sql query

--- server/api/models.py
+++ server/api/models.py
@@ -2,6 +2,9 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+    class Meta:
+        db_table = 'users'  # Set the table name to 'user'
+
     username = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
