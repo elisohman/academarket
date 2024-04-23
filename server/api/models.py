58d3475d82ff8358849ac4fc59c46c787faa7af3
jacko460 <jacko460@student.liu.se>
2024-04-23 17:27:00 +0200

    add update functionality for new course exams to local data, add course to models, minor fixes and cleanup

--- server/api/models.py
+++ server/api/models.py
@@ -9,3 +9,11 @@ class User(models.Model):
     email = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     coins = models.IntegerField(null=True)
+
+class Course(models.Model):
+    class Meta:
+        db_table = 'courses'  # Set the table name to 'user'
+    id = models.AutoField(primary_key=True)
+    course_code = models.CharField(max_length=10, unique=True)
+    course_name = models.CharField(max_length=30)
+    course_description = models.CharField(max_length=500)
--- server/api/views.py
+++ server/api/views.py
@@ -5,10 +5,12 @@ import json
 import requests
 from api.utils.handle_local_json import read_course_data_from_code, read_all_course_data, check_if_course_exists_locally, save_course_data
 
-# Account related (sign in, sign out, etc).
+#-- Views! --#
+
 def test(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello world. :)")
 
+# Account related views (sign in, sign out, etc).
 
 def sign_in(request: HttpRequest) -> HttpResponse:
     """
@@ -28,12 +30,15 @@ def sign_in(request: HttpRequest) -> HttpResponse:
         password = dictionary['password']
     else:
         # Use an empty password if none is provided
+
         password = ""
     user = User.objects.filter(username=username).first()
     if user:
         if user.password == password:
-            return HttpResponse(status=200, content="Hello world. :)")
-    return HttpResponse(status=400, content="Big error")
+            return HttpResponse(status=200, content="User logged in!")
+        return HttpResponse(status=403, content="Wrong password.")
+
+    return HttpResponse(status=404, content="User does not exist.")
 
 
 def sign_up(request: HttpRequest) -> HttpResponse:
@@ -53,7 +58,7 @@ def sign_up(request: HttpRequest) -> HttpResponse:
     if "password" in dictionary:
         password = dictionary['password']
     else:
-        # Use an empty password if none is provided
+        # Use an empty password if none is provided. Temporary solution!
         password = ""
     if User.objects.filter(username=username).exists():
         return HttpResponse(status=403, content="No!")
@@ -62,44 +67,56 @@ def sign_up(request: HttpRequest) -> HttpResponse:
     return HttpResponse(status=200, content="New user created!")
 
 
-# Stealing from ysektionen
+# Views for robbing y-sektionen
 
-def get_course_stats(request: HttpRequest, course_code: str) -> HttpResponse:
-    # Define the URL of the API endpoint
-    params = request.GET.dict()
+def get_course_stats(_request: HttpRequest, course_code: str) -> JsonResponse:
+    """
+    Stealing statistics for a given course from a certain API, saving it locally if necessary.
+
+    Args:
+        _request (HttpRequest): The HTTP request object.
+        course_code (str): The code of the course for which to retrieve the statistics.
+
+    Returns:
+        JsonResponse: The JSON response containing the course statistics.
+
+    Raises:
+        JsonResponse: If there is an error retrieving or saving the course statistics.
+    """
+    should_update = False # Temporary placement, for when we want to check for new exams in courses already in the database
     api_url = f"https://ysektionen.se/student/tentastatistik/exam_stats/?course_code={course_code}&newer_than=2012&month_lb=&month_ub="
     try:
-        #if not read_all_course_data():
-        #    course_code = "TATA24" # just to make sure it creates the json file correctly
-            #return HttpResponse(status=500, content="Get out!")
-        if check_if_course_exists_locally(course_code):
+        if check_if_course_exists_locally(course_code) and not should_update:
             data = read_course_data_from_code(course_code)
             return JsonResponse(data, status=200)
-        # Make a GET request to the API
+        
+        # Making a GET request to the API
         response = requests.get(api_url)
 
-        # Check if the request was successful
         if response.status_code == 200:
-            # Parse JSON response
             data = response.json()
             
             save_course_data(data)
 
-            # Return the JSON response
-            return JsonResponse(data)
+            return JsonResponse(data, status=200)
         else:
-            # If the request was not successful, return an error response
             return JsonResponse({'error': 'Failed to fetch data from the API'}, status=500)
     except Exception as e:
-        # If an exception occurs, return an error response
         return JsonResponse({'error': str(e)}, status=500)
 
 
-def get_local_course_data(request):
-    # Define the path to the JSON file
+def get_local_course_data(_request: HttpRequest) -> JsonResponse:
+    """
+    Retrieve local course data and return it as a JSON response.
+
+    Parameters:
+    - _request (HttpRequest): The HTTP request object. Unused.
+
+    Returns:
+    - JsonResponse: The JSON response containing the local course data.
+
+    """
     local_courses_data = read_all_course_data()
-    # Check if the JSON file exists
     if local_courses_data:
         return JsonResponse(local_courses_data, status=200)
     return JsonResponse({'error': 'No data available'}, status=404)
-
