a5c70ba0792e83ea2031829d790eadbbbb4b5002
Johannes Pettersson <johpe243@student.liu.se>
2024-04-23 16:23:32 +0200

    top bar is now fully functional (except profile icon/button)

--- client/src/components/topbar/TopBar.tsx
+++ client/src/components/topbar/TopBar.tsx
@@ -1,20 +1,31 @@
-
+import {Link, useLocation} from "react-router-dom";
 
 function TopBar() {
+    const location = useLocation();
+    const path = location.pathname;
+
     return (
-        <div className="bg-slate-600 w-full h-20 flex px-4">
+        <div className="text-secondary-color w-full h-20 flex px-8 text-base">
             <div className="flex items-center">
-                <div className="text-white text-2xl font-bold">Logo</div>
+                <div className="text-lg font-bold">Logo</div>
             </div>
             <div className="flex-grow flex justify-center">
-                <ul className="flex space-x-4 text-white items-center">
-                    <li>Dashboard</li>
-                    <li>Portfolio</li>
-                    <li>Trading</li>
+                <ul className="flex gap-x-8 items-center">
+                    <Link to="/dashboard">
+                        <li className={`px-4 py-1.5 rounded-full ${path === '/dashboard' ? 'bg-primary-color text-white font-medium' : ''}`}>Dashboard</li>
+                    </Link>
+
+                    <Link to="/portfolio">
+                        <li className={`px-4 py-1.5 rounded-full ${path === '/portfolio' ? 'bg-lavender text-white font-medium' : ''}`}>Portfolio</li>
+                    </Link>
+
+                    <Link to="/trading">
+                        <li className={`px-4 py-1.5 rounded-full ${path === '/trading' ? 'bg-coral text-white font-medium' : ''}`}>Trading</li>
+                    </Link>
                 </ul>
             </div>
             <div className="flex items-center">
-                <div className="text-white text-lg">User Profile</div>
+                <div className="w-10 h-10 rounded-full bg-light-gray"></div>
             </div>
         </div>
     );
