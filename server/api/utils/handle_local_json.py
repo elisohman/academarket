ca568d98e57e733e0f6aac73f3a6c27e78cf8fb0
jacko460 <jacko460@student.liu.se>
2024-04-23 17:27:00 +0200

    add update functionality for new course exams to local data, add course to models, minor fixes and cleanup

--- server/api/utils/handle_local_json.py
+++ server/api/utils/handle_local_json.py
@@ -64,4 +64,48 @@ def save_course_data(data):
             json.dump(local_courses_data, file, ensure_ascii=False, indent=4)
             print("Data saved successfully to courses.json")
     else:
+        update_course_data(course_code, data)
         print("Data already exists in courses.json")
+
+def update_course_data(course_code, data):
+    """
+    Here we want to add new exams to the course code if 
+    new results for that has been released that is not 
+    present in courses.json
+    """
+    # Define the path to the JSON file
+    json_file_path = 'courses.json'
+    
+    # Create an empty dictionary to store course data
+    local_courses_data = {}
+    
+
+    # Check if the JSON file exists
+    if os.path.exists(json_file_path):
+        # Read existing JSON data from the file
+        with open(json_file_path, 'r', encoding='utf-8') as file:
+            local_courses_data = json.load(file)
+    
+    exams_data = data.get('exams')
+    for module_key in exams_data:
+        print(module_key)
+        # Checking if any examination modules needs to be added or updated
+        if module_key not in local_courses_data[course_code]['exams']:
+            local_courses_data[course_code]['exams'][module_key] = exams_data[module_key]
+        else:
+            for date_index, date_key in enumerate(exams_data[module_key]):
+                if date_key not in local_courses_data[course_code]['exams'][module_key]:
+                    local_courses_data[course_code]['exams'][module_key][date_key] = exams_data[module_key][date_key]
+
+                    for data_index, data_dict in enumerate(exams_data[module_key]["data"]):
+                        local_courses_data[course_code]['exams'][module_key]["data"][data_index]["data"].insert(data_dict[date_index], date_index)
+
+    # Write new JSON data back to the file
+    if local_courses_data.get(course_code) is not None:
+        local_courses_data[course_code].update(data)
+        # Write updated JSON data back to the file
+        with open(json_file_path, 'w', encoding='utf-8') as file:
+            json.dump(local_courses_data, file, ensure_ascii=False, indent=4)
+            print("Data updated successfully in courses.json")
+    else:
+        print("Course data not found in courses.json")
