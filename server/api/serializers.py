38dbaf2129a5305b386cb8345ad04aace221d40d
jacko460 <jacko460@student.liu.se>
2024-06-06 14:28:57 +0200

    change names for seralizers to better correspond with rest of code

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import User
 
-class RegisterSerializer(serializers.ModelSerializer):
+class SignUpSerializer(serializers.ModelSerializer):
     rpt_password = serializers.CharField(write_only=True)
 
     class Meta:
@@ -12,7 +12,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'] 
         )
         user.set_password(validated_data['password'])
         user.save()
@@ -23,7 +23,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({'password': 'Passwords must match.'})
         return data
         
-class LoginSerializer(serializers.Serializer):
+class SignInSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
