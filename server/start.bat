100b5a3842d7aaea1275ff09b4b537ed516e692d
unknown <jack.kolm@outlook.com>
2024-06-18 13:23:13 +0200

    correct start script

--- server/start.bat
+++ server/start.bat
@@ -1,4 +1,8 @@
-python manage.py runserver 0.0.0.0:8000
-# Start server (accessable on devices across wifi!)
-echo "Starting server..."
+@echo off
+REM Start venv
+echo Activating server...
+call .venv\Scripts\activate
+
+REM Start server (accessible on devices across Wi-Fi!)
+echo Starting server...
 python manage.py runserver 0.0.0.0:8000
