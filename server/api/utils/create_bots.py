b279a796c63cc4b24ba7975668c502939624c3a5
jacko460@student.liu.se <jacko460@student.liu.se>
2024-08-04 10:09:09 +0200

    add empty botnames.txt file

--- server/api/utils/create_bots.py
+++ server/api/utils/create_bots.py
@@ -1,7 +1,7 @@
 from api.models import User, Portfolio
 import random
 
-def create_bot_user(name: str, email: str) -> None:
+def create_bot_user_in_db(name: str, email: str) -> None:
     """
     Create a bot user for the database.
     """
@@ -11,6 +11,13 @@ def create_bot_user(name: str, email: str) -> None:
         new_portfolio = Portfolio(user=bot_user)
         new_portfolio.save()
 
+def add_bot_name_to_txt(name: str) -> None:
+    """
+    Add the newly created bot's name to the botnames.txt file in resources/
+    """
+    with open('api/utils/resources/botnames.txt', 'a') as f:
+        f.write(name + "\n")
+    
 
 def get_names_from_files() -> tuple:
     """
@@ -26,12 +33,12 @@ def get_names_from_files() -> tuple:
 def create_bots() -> None:
     firstnames, surnames = get_names_from_files()
     # Get a random first and a random surname and concatenate them
-    create_bot_user("Jamal Davis", "jamaldavis@daviscompany.com")
+    create_bot_user_in_db("Jamal Davis", "jamaldavis@daviscompany.com")
     for i in range(100):
         first_name = firstnames[random.randint(0, len(firstnames)-1)].strip()
         surname = surnames[random.randint(0, len(surnames)-1)].strip()
         full_name = first_name + " " + surname
         email = first_name + surname + "@botmail.com"
         #print(name)
-        create_bot_user(full_name, email)
+        create_bot_user_in_db(full_name, email)
 
